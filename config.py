76db94aef269032a88ee20a239484924a997c92b
danielns <danieln.silva@yahoo.com.br>
2014-07-02 21:38:00 -0300

    Banco de dados compativel com heroku

--- config.py
+++ config.py
@@ -6,5 +6,9 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 CSRF_ENABLED = True
 SECRET_KEY = 'you-will-never-guess'
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') # caminho do banco app.db
+if os.environ.get('DATABASE_URL') is None:
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db') # caminho do banco app.db
+else:
+    SQLALCHEMY_DATABASE_URI = os.environ['DATABASE_URL']
+
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository') # caminho do repositório
